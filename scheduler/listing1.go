b0e7f01028465f472c8771ccff96ba455102dabf
Dmitry <omelya.emelya@gmail.com>
2018-08-23 16:43:19 +0300

    go scheduler part 1

---
+++ scheduler/listing1.go
@@ -0,0 +1,5 @@
+goroutine 1 [running]:
+   main.example(0xc000042748, 0x2, 0x4, 0x106abae, 0x5, 0xa)
+       stack_trace/example1/example1.go:13 +0x39                 <- LOOK HERE
+   main.main()
+       stack_trace/example1/example1.go:8 +0x72                  <- LOOK HERE
